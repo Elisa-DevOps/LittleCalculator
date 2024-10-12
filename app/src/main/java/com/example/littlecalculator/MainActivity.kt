c3dc26a9a297af2ac4fc527f4baf7a2fc8a18cb9
Elisa <elisa-devops@users.noreply.github.com>
2024-10-12 09:46:40 -0500

    created handle button function and rendered to text view solution output

--- app/src/main/java/com/example/littlecalculator/MainActivity.kt
+++ app/src/main/java/com/example/littlecalculator/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.littlecalculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +21,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun handleButton(view: View) {
+        val editTextNumDeci = findViewById<EditText>(R.id.editTextNumDeci)
+        val textViewSoluOutput = findViewById<TextView>(R.id.textViewSoluOutput)
+
+        val numDeci = editTextNumDeci.text.toString().toDoubleOrNull()
+        if (numDeci != null) {
+            val solu = calculateSolution(numDeci)
+            textViewSoluOutput.text = solu.toString()
+        } else {
+            textViewSoluOutput.text = "Invalid input"
+        }
+
+    }
+
+    private fun calculateSolution(numDeci: Double): Any {
+        var solu = 0.0
+        for (i in 1..numDeci.toInt()) {
+            solu += 1.0 / i
+        }
+        return solu
     }
 }
