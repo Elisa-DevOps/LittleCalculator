5ab7a3c31deb9e73e1496ad5f11d92e1bb9c8b7c
Elisa <elisa-devops@users.noreply.github.com>
2024-10-14 09:37:39 -0500

    added logic to kotlin file for multiply and divide

--- app/src/main/java/com/example/littlecalculator/MainActivity.kt
+++ app/src/main/java/com/example/littlecalculator/MainActivity.kt
@@ -5,11 +5,8 @@
 package com.example.littlecalculator
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
-import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -29,24 +26,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun handleButton(view: View) {
+    fun handleButton() {
         val editTextNumDeci = findViewById<EditText>(R.id.editTextNumDeci)
         val textViewSoluOutput = findViewById<TextView>(R.id.textViewSoluOutput)
 
         val numDeci = editTextNumDeci.text.toString().toDoubleOrNull()
         val soluDeci = textViewSoluOutput.text.toString().toDoubleOrNull()
 
-            if (numDeci != null) {
-                val solu = calculateSolution(numDeci, soluDeci)
-                textViewSoluOutput.text = solu.toString()
-            } else {
-                textViewSoluOutput.text = "Invalid input"
-            }
+        if (numDeci != null) {
+            val solu = calculateSolution(numDeci, soluDeci)
+            textViewSoluOutput.text = solu.toString()
+        } else {
+            textViewSoluOutput.text = getString(R.string.invalid_input)
+        }
     }
 
     private fun calculateSolution(numDeci: Double?, soluDeci: Double?): Double {
         val plusRadioButton = findViewById<RadioButton>(R.id.plusRadioButton)
         val minusRadioButton = findViewById<RadioButton>(R.id.minusRadioButton)
+        val multiplyRadioButton = findViewById<RadioButton>(R.id.multiplyRadioButton)
+        val divideRadioButton = findViewById<RadioButton>(R.id.divideRadioButton)
 
         var solu = numDeci!!
 
@@ -54,6 +53,10 @@ class MainActivity : AppCompatActivity() {
                 solu = soluDeci!! + numDeci
             } else if (minusRadioButton.isChecked) {
                 solu = soluDeci!! - numDeci
+            } else if (multiplyRadioButton.isChecked) {
+                solu = soluDeci!! * numDeci
+            } else if (divideRadioButton.isChecked) {
+                solu = soluDeci!! / numDeci
             }
         return solu
     }
